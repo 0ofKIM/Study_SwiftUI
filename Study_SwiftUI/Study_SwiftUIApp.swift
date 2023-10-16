cf2ab8c8ef514567b6464af1c8150703ef320820
0ofKim <pos00042@naver.com>
2023-10-16 09:38:30 +0900

    first commit

---
+++ Study_SwiftUI/Study_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_SwiftUIApp.swift
+//  Study_SwiftUI
+//
+//  Created by 0ofKim on 2023/10/16.
+//
+
+import SwiftUI
+
+@main
+struct Study_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
